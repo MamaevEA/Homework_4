fd7655dd48be9567f8c425e4b85b7a8ef331c819
MamaevEA <mamaevea@gmail.com>
2022-08-12 17:40:58 +0500

    Add Task23

---
+++ Homework_4_2/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое число от 1 до N для нахождения кубов всех чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
+{
+    Console.WriteLine($" {count} -> {Math.Pow(count, 3)}");
+    count++;
+}
