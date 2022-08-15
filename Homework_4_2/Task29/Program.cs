65dce83b5774c6eaa27648c7ed0ffa810b8d487a
MamaevEA <mamaevea@gmail.com>
2022-08-15 23:25:00 +0300

    Add Task29

--- Homework_4_2/Task29/Program.cs
+++ Homework_4_2/Task29/Program.cs
@@ -2,3 +2,9 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 
+int [] array = new int [8];
+
+for (int i = 0; i < array.Length; i++)
+   array[i] = new Random().Next(0,100);
+
+Console.WriteLine($"Массив из 8 псевдослучайных целых чисел от 0 до 100: [{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}, {array[5]}, {array[6]}, {array[7]}]");
