d8733c01d38d4f51b89cce28d1a4f89575bb69f9
MamaevEA <mamaevea@gmail.com>
2022-08-15 22:53:17 +0300

    Edit Task25

--- Homework_4_2/Task25/Program.cs
+++ Homework_4_2/Task25/Program.cs
@@ -7,4 +7,13 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число В, обозначающее степень, в которую необходимо будет возвести число А: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"{a} возведенное в {b} степень ровняется: {Math.Pow(a, b)}");
+int result = 1;
+int i = 1;
+
+while (i <= b)
+{
+    result = result * a;
+    i++;
+}
+
+Console.WriteLine($"{a} возведенное в {b} степень ровняется: {result}");
