8a05972f7d5493423918f3d8dac95f520343cb3e
MamaevEA <mamaevea@gmail.com>
2022-08-29 10:40:26 +0500

    Add Task36

---
+++ Homework_4_2/Task36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+
+
+int [] array = new int [8];
+int summ = 0;
+int i = 0;
+
+for (i = 0; i < array.Length; i++)
+   array[i] = new Random().Next(-100,100);
+
+i = 1;
+
+while (i < array.Length)
+summ = summ + array[i];
+i = i + 2;
+
+
+Console.WriteLine($"Массив из 8 псевдослучайных целых чисел от -100 до 100: ");
+for (i = 0; i < array.Length; i++)
+   Console.Write($"{array[i]}, ");
+
+Console.WriteLine($" ");
+
+Console.WriteLine($"Cуммa элементов, стоящих на нечётных позициях (индексах): {summ}");
