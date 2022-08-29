5f6df45732b2c1a2695778559dd7b40008a2c761
MamaevEA <mamaevea@gmail.com>
2022-08-29 12:25:18 +0500

    Add Task43

---
+++ Homework_4_2/Task43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите координату k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+x = ((b2 - b1) / (k1 - k2));
+y = ((k1 * x) + b1);
+
+Console.WriteLine($"Координаты точки пересечения: ({x}), ({y})");
