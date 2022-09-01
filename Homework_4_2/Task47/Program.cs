3e103a06884cc079557dd62cbc1f9c81e76b13bb
MamaevEA <mamaevea@gmail.com>
2022-09-01 15:35:57 +0500

    Add Task47

---
+++ Homework_4_2/Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (double[,] matr)
+{
+     for (int i = 0; i < matr.GetLength(0); i++)
+     {
+         for (int j = 0; j < matr.GetLength(1); j++)
+         {
+             matr[i,j] = new Random().NextDouble()*10;
+         }
+     }
+ }
+
+int m = 3;
+int n = 4;
+
+double[,] matrix = new double[m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
