c42dc32419e1165cf9bab03544dce3bb99c0a985
MamaevEA <mamaevea@gmail.com>
2022-09-01 15:54:20 +0500

    Add Task50

---
+++ Homework_4_2/Task50/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] mass = new int[,]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+
+Console.WriteLine($"Введите номер строки");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите номер столбца");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x < mass.GetLength(0) && y < mass.GetLength(1))
+{
+    Console.WriteLine($"{mass[x,y]}");
+}
+else Console.WriteLine($"такого элемента в массиве нет");
+
